8289a5e30bc92dc6086c80408dfc4454a5130a4f
hatetavian <tavianjdawson123@gmail.com>
2021-11-17 13:29:53 -0600

    finishing touches

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,19 +55,6 @@ $("#currentDay").text(moment().format("dddd, MMMM Do"))
 
 
 
-$("#searchBtn").on("click", function(){
-    var searchVal = $("#cityInput").val()
-
-    getCurrentWeather(searchVal)
-})
-
-function getCurrentWeather(searchVal) {
-console.log("SERACH VAL IN GETCURRENT WEATHER", searchVal)
-// youll call your api and input the searchVal into the queryURL
-}
-
-
-
 
 
 
